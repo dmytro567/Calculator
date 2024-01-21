958707e1ac4708d198b2e5194f7f9a9678335854
dmytro567 <122375607+dmytro567@users.noreply.github.com>
2024-01-21 21:56:33 +0200

    Update 5.3.3.js

--- 5.3/5.3.3.js
+++ 5.3/5.3.3.js
@@ -1,6 +1,6 @@
 function truncate(str, maxlength) {
   if (str.length > maxlength) {
-    return str.slice(0, maxlength - 3) + "...";
+    return str.slice(0, maxlength - 3) + "..."; //цей рядок піддивився бо не було повного пояснення роботи slice
   } else {
     return str;
   }
